295f7d335bc55cb85f8e1117111628479e79ff5c
fnyaoke <fnyaoke@gmail.com>
2020-11-07 06:56:11 +0300

    set up for deployment to heroku

--- config.py
+++ config.py
@@ -1,14 +1,12 @@
 import os
 
 class Config:
-
+    SQLALCHEMY_DATABASE_URI ='postgres://moringa:Access@localhost:5432/pitches'
+    SQLALCHEMY_TRACK_MODIFICATIONS=True
     SECRET_KEY = 'my12345key67890secret'
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/blog'
-
-
     #  email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
@@ -19,8 +17,8 @@ class Config:
 class ProdConfig(Config):
 
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/blogspot'
-    #SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/blogspot'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class DevConfig(Config):
